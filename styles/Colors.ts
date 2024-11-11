b2830cd7cc9847e1074231e7538a80a0035a64a9
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-11-11 20:05:55 +0100

    refact(Colors): Changed colors for dark theme and rename its properties

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -1,16 +1,14 @@
 export const COLORS_DARK = {
-  backgroundPrimary: "#3E2723",
-  backgroundSecondary: "#5D4037",
-  backgroundTertiary: "#4E342E",
-  textPrimary: "#FFEBEE",
-  textSecondary: "#FFCDD2",
-  borderColor: "#FFEBEE",
+  backgroundPrimary: "#263238",
+  backgroundSecondary: "#37474F",
+  textPrimary: "#ECEFF1",
+  textSecondary: "#CFD8DC",
+  borderColor: "#ECEFF1",
 };
 
 export const COLORS_LIGHT = {
-  backgroundPrimary: "#FFF7E6",
-  backgroundSecondary: "#FFD699",
-  backgroundTertiary: "#FFF0DB",
+  backgroundPrimary: "#FFD699",
+  backgroundSecondary: "#FFF7E6",
   textPrimary: "#4E342E",
   textSecondary: "#6D4C41",
   borderColor: "#4E342E",
