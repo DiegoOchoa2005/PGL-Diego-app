e43ba10230f25b861212e3396591e8f5a9b2411e
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-11-11 23:19:26 +0100

    refact(Colors): United light and dark themes in one

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -1,15 +1,18 @@
-export const COLORS_DARK = {
-  backgroundPrimary: "#263238",
-  backgroundSecondary: "#37474F",
-  textPrimary: "#ECEFF1",
-  textSecondary: "#CFD8DC",
-  borderColor: "#ECEFF1",
+const theme = {
+  light: {
+    backgroundPrimary: "#FFD699",
+    backgroundSecondary: "#FFF7E6",
+    textPrimary: "#4E342E",
+    textSecondary: "#6D4C41",
+    borderColor: "#4E342E",
+  },
+  dark: {
+    backgroundPrimary: "#263238",
+    backgroundSecondary: "#37474F",
+    textPrimary: "#ECEFF1",
+    textSecondary: "#CFD8DC",
+    borderColor: "#ECEFF1",
+  },
 };
 
-export const COLORS_LIGHT = {
-  backgroundPrimary: "#FFD699",
-  backgroundSecondary: "#FFF7E6",
-  textPrimary: "#4E342E",
-  textSecondary: "#6D4C41",
-  borderColor: "#4E342E",
-};
+export default theme;
