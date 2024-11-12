e96d30b2f402c2a93f0491c17c4c14fbdf7bab66
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-11-12 01:14:59 +0100

    feat(index): Added portfolo page

---
+++ app/profiles/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+const PortfolioPage = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
+};
+
+export default PortfolioPage;
+
+const styles = StyleSheet.create({});
