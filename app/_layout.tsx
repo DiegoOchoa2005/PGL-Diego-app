3340474e65da5da9ed96ebee8171cf5aac187093
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-01-25 18:12:05 +0100

    feat(_layout):Added Drawer logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
-import { Slot } from "expo-router";
 import ThemeProvider from "../provider/ThemeProvider";
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const AppLayout = () => {
   return (
-    <ThemeProvider>
-      <View style={styles.container}>
-        <Slot />
-      </View>
-    </ThemeProvider>
+    <GestureHandlerRootView style={styles.container}>
+      <ThemeProvider>
+        <Drawer
+          screenOptions={{
+            headerStyle: {
+              height: 60,
+            },
+          }}
+        >
+          <Drawer.Screen
+            name="index"
+            options={{
+              title: "App :3",
+              drawerLabel: "Home",
+            }}
+          />
+          <Drawer.Screen
+            name="portfolio"
+            options={{
+              title: "App :3",
+              drawerLabel: "Portfolio",
+            }}
+          />
+          <Drawer.Screen
+            name="shoppinglist/index"
+            options={{
+              title: "App :3",
+              drawerLabel: "Lista de compras",
+            }}
+          />
+        </Drawer>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 };
 
